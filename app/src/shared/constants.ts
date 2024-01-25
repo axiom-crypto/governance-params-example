4b7de814ef4c9749ca9e59589f19df01d6765495
Yu Jiang Tham <yoojbruin@gmail.com>
2024-01-25 00:30:38 -0500

    Use verified contracts

--- app/src/shared/constants.ts
+++ app/src/shared/constants.ts
@@ -2,8 +2,8 @@ export const Constants = Object.freeze({
   EXPLORER_BASE_URL: "https://explorer.axiom.xyz/v2/sepolia",
   CHAIN_ID_SEPOLIA: 11155111,
 
-  NFT_ADDR: "0x68A2AC53CA82203c7adD1A7778EC781ea41Dd6B9",
-  GOVERNANCE_ADDR: "0xc1c981Eda4eb2BDB7e4550bC6447E9bfD178D32a",
+  NFT_ADDR: "0x82e2323c00298D9F19373127f620DA38381B1917",
+  GOVERNANCE_ADDR: "0xd372c836DBCbe25B183373bdc82F0392C4F1A5A4",
 
   // Transfer (index_topic_1 address from, index_topic_2 address to, index_topic_3 uint256 tokenId)
   TRANSFER_EVENT_SCHEMA: "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
